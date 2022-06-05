3f96d56fcbf5874ed6af82b4302595dbe5e446f1
youngentry <90388461+youngentry@users.noreply.github.com>
2022-06-05 23:22:39 +0900

    220603일차 sticky+js scrollTop
    sticky를 알고 싶다면 여기.

--- js/position03.js
+++ js/position03.js
@@ -1,1 +1,14 @@
-$(function () {});
+$(function () {
+  $("header i").on("click", function () {
+    $(".cover").toggleClass("on");
+  });
+
+  $(".cover a").on("click", function (e) {
+    e.preventDefault();
+    let idx = $(this).parent().index();
+    console.log(100 * idx + "%", $(".sticky").height());
+    let h = $(".sticky").height();
+    $("html").animate({ scrollTop: (idx + 1) * h }, 1000);
+    /* 본래는 html, body */
+  });
+});
