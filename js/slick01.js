1f8f384376a48595d83a7991f55d76a129f4a0a4
youngentry <gentry_@naver.com>
2022-06-13 14:13:12 +0900

    220613 슬라이드복습 마우스휠

---
+++ js/slick01.js
@@ -0,0 +1,20 @@
+$(function () {
+  $(".visual_slider").slick({
+    dots: true,
+    autoplay: true,
+    pauseOnHover: false,
+  });
+
+  $(".slider").slick({
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 4000,
+    speed: 1000,
+    dots: true,
+    pauseOnHover: false,
+  });
+
+  $(".slider02").slick({
+    slidesToShow: 3,
+  });
+});
