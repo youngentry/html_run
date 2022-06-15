0de7cbedcea48e8a7bd8257b53b5b053ff415e59
youngentry <gentry_@naver.com>
2022-06-15 11:51:38 +0900

    220615 올라가는 슬라이드번호 background 활용 ★★★

--- js/slick03.js
+++ js/slick03.js
@@ -1,16 +1,20 @@
 $(function () {
+  let createNum = 0;
+
   $(".main_slider").on("init afterChange", function (e, s, c) {
     $(".main_slider figure.slick-current")
       .addClass("on")
       .siblings()
       .removeClass("on");
+    $(".num").css({ backgroundPositionY: -100 * createNum + "px" });
+    createNum++;
   });
 
   $(".main_slider").slick({
     arrows: false,
-    dots: true,
     autoplay: true,
     autoplaySpeed: 4000,
     pauseOnHover: false,
+    pauseOnFocus: false,
   });
 });
