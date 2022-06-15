1dab44c3d1d4d136e9f1dea4700c46f2ff06a12d
youngentry <gentry_@naver.com>
2022-06-15 17:13:11 +0900

    220615 slide page count up, slide gage

--- js/slick03_copy.js
+++ js/slick03_copy.js
@@ -1,32 +1,40 @@
 $(function () {
-  var createNum = 0;
+  var createNum = -1;
 
   $(".main_slider").on("init afterChange", function (e, s, c) {
     $(".main_slider figure.slick-current")
       .addClass("on")
       .siblings()
       .removeClass("on");
+    if (createNum % 3 == c) {
+      createNum = createNum;
+    } else if ((createNum % 3) - c == 1) {
+      createNum--;
+    } else if ((createNum % 3) - c == 2) {
+      createNum++;
+    } else if ((createNum % 3) - c == -2) {
+      createNum = createNum + 2;
+    } else {
+      createNum++;
+    }
     $(".num").css({ backgroundPositionY: -100 * createNum + "px" });
     $(".custom_dots li").eq(0).addClass("on");
     $(".custom_dots li").eq(c).addClass("on").siblings().removeClass("on");
-    createNum++;
-    console.log(createNum);
+    console.log(c, createNum, createNum % 3);
   });
 
   $(".main_slider").slick({
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 1000,
     pauseOnHover: false,
     pauseOnFocus: false,
+    autoplaySpeed: 4000,
   });
 
   $(".custom_dots a").on("click", function (e) {
     e.preventDefault();
     var idx = $(this).parent().index();
     $(".main_slider").slick("slickGoTo", idx);
-    $(".num").css({ backgroundPositionY: -100 * idx + "px" });
-    createNum = idx;
   });
 
   $(".custom_arrow i:nth-child(1)").on("click", function () {
